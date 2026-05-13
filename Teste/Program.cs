a5060b074bcc7ae9b1c17662cada779c396408f8
Jefferson Riann Meira Santos <235415849+DevRiann@users.noreply.github.com>
2026-05-12 22:49:37 -0300

    Update

---
+++ Teste/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(" Desconto de valores ");
+
+Console.Write("Digite o nome do produto: ");
+string produto = Console.ReadLine();
+Console.Write("Qual o valor total: ");
+double valor = double.Parse(Console.ReadLine());
+
+if(valor < 100)
+{
+    Console.WriteLine("Este produto não tem desconto");
+}
+else if(valor < 500)
+{
+    double desconto = valor - (valor * 0.15);
+    Console.WriteLine($"O produto {produto} tem um desconto de 15%, seu total será R$ {desconto}");
+}
+else
+{
+    double desconto = valor - (valor * 0.3);
+    Console.WriteLine($"O produto {produto} tem um desconto de 30%, seu total será R$ {desconto}");
+}
+
+
+
+
+
